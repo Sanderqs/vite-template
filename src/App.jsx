778f859ea6d74248689ee41b4a250c2090826b3a
Sanderqs <channie.qs@gail.com>
2025-04-09 00:16:25 +0200

    setState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import DrinkButtons from "./components/DrinkButtons";
 import DrinkChoise from "./components/DrinkChoise";
 import { tea, coffee } from "./utils/data";
 const App = () => {
   const greeting = "Welcome to our cafe!";
-  const userDrink = coffee;
+  const [userDrink, setUserDrink] = useState();
   return (
     <>
       <div className="app">
-        <h1>{greeting}</h1>
-        <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
-        <DrinkChoise drink={userDrink} />
+        {userDrink ? (
+          ""
+        ) : (
+          <>
+            <DrinkButtons drinkOne={tea.name} drinkTwo={coffee.name} />
+            <h1>{greeting}</h1>
+          </>
+        )}
+        {userDrink && <DrinkChoise drink={userDrink} />}
       </div>
     </>
   );
--- src/components/DrinkChoise.jsx
+++ src/components/DrinkChoise.jsx
@@ -1,9 +1,11 @@
 const DrinkChoise = ({ drink }) => {
   return (
     <>
-      <p>{drink.name}</p>
-      <img src="" alt={drink.name} width="100px" height="100px" />
-      <p>Drink will be ready in a few minutes</p>
+      <div>
+        <p>{drink.name}</p>
+        <img src={drink.imgUrl} alt={drink.name} width="100px" height="100px" />
+        <p>Drink will be ready in a few minutes</p>
+      </div>
     </>
   );
 };
